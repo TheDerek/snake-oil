6bf1f0ad1bb1d019a8f38aacabcca56487cecdb8
Derek <mail@derek.tech>
2016-12-22 23:05:01 +0000

    Regigged cards.

--- card_maker.py
+++ card_maker.py
@@ -1,12 +1,28 @@
-import xml.etree.ElementTree
+import xml.etree.ElementTree as ET
 
 
 if __name__ == "__main__":
-    # Load the svg file
-    svg = xml.etree.ElementTree.parse('assets/word-front.svg').getroot()
 
-    # Find the text we need to replace
-    word_element = svg.find('.//*[@id="wordText"]')
-    word_element.text = "OPera"
+    # Load the list of words
+    wordsFile = open('assets/words.txt')
+    words = list(map(lambda w: w.strip(), wordsFile.readlines()))
 
-    print(word_element.text)
+    # Create an SVG for each card with a customised word
+    for word in words:
+        # Load the svg file
+        root = ET.parse('assets/word-front.svg').getroot()
+
+        # Find the text we need to replace
+        word_element = root.find('.//*[@id="wordText"]')
+        word_element.text = word.capitalize()
+
+        # Save the file
+        path = 'assets/cards/' + word + '.svg'
+        tree = ET.ElementTree(root)
+        tree.write(path)
+
+
+
+
+
+    print(words)
