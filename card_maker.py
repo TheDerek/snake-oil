c013421acfe5328750a4a7ee63247528943b2c97
Derek <mail@derek.tech>
2016-12-22 22:36:04 +0000

    Added basic detection.

--- card_maker.py
+++ card_maker.py
@@ -1,1 +1,12 @@
 import xml.etree.ElementTree
+
+
+if __name__ == "__main__":
+    # Load the svg file
+    svg = xml.etree.ElementTree.parse('assets/word-front.svg').getroot()
+
+    # Find the text we need to replace
+    word_element = svg.find('.//*[@id="wordText"]')
+    word_element.text = "OPera"
+
+    print(word_element.text)
